394505fe2b71d01f421a3e21f7178f11dcf8416d
ajruben <aj.rubenswarts@gmail.com>
2024-02-23 12:07:03 +0100

    world 1 room 1 ontwikkelen, key scenario bijna klaar

--- main.py
+++ main.py
@@ -48,18 +48,42 @@ def show_gravity_activity():
 #rooms world 1
 def w1_r1(): #world one room one
     welcome_text_room1 = """
-    Welcome in the world of physics, where the physics is theoretical but the fun is real!
-    You are in a world where you can control gravity, initially there is no gravity. You start in a room with a bunch of stuff floating around.
+    Welcome in the world of theoretical physics, where the physics is theoretical but the fun is real!
+    You are in a world where you can control gravity, initially there is no gravity. You start in a room with a bunch of stuff floating about.
     There is a locked door, the key is floating on the other side of the room.
     Would you like to open the door with brute force, or try and get the key?
     option A: brute force
-    option B: try get key
+    option B: try get the key
     """
-    keyforce_choice = input(welcome_text_room1)
+    keyforce_choice = None
+    while keyforce_choice == None:
+        keyforce_choice = input(welcome_text_room1)
+        if keyforce_choice == "A":
+            w1r1_brute()
+        elif keyforce_choice == "B":
+            w1r1_get_key()
+        else:
+            print("Error: please enter either 'A' or 'B'.")
+            keyforce_choice = None
+
 
 def w1r1_get_key():
-    pass
-
+    first_text_key = """
+    Right now we can't reach the key. Luckily we can control the gravity with our gravity gadget.
+    One of the features is the gravity toggler, which can toggle gravity on and off.
+    Do you want to turn the gravity on? (a = g =  9.81 ms^2)
+    Option A: yes
+    Option B: no
+    please type 'A' or 'B' to confirm your choice.
+    """
+    toggle_choice = None
+    while toggle_choice == None:
+        toggle_choice = input(first_text_key)
+        if toggle_choice == "A":
+            print("Everything falls to the ground. Including you (ouch). Luckily we can still walk, lets grab the key!")
+        elif toggle_choice == "B":
+            print("Our gravity gadget inlcudes other features! We could also use the gravity ray to pull objects towards us (and us towards the object, considering Newtons third law).")
+            
 def w1r1_brute():
     pass
 
